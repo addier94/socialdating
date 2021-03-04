36414fcac3ebebded43ec453e8b182970420ec1d
Alfredo Fernandez <alfredo@hotmail.com>
2021-03-04 17:30:18 -0400

    test for auth middleware

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\StatusesController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('statuses', [StatusesController::class, 'store'])->name('statuses.store');
+Route::post('statuses', [StatusesController::class, 'store'])->name('statuses.store')->middleware('auth');

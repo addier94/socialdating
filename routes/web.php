ced293e6f89640133752dd3cf83dffe5cb983e9b
Alfredo Fernandez <alfredo@hotmail.com>
2021-03-04 22:39:31 -0400

    before delete content of welcome.blade.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::view('/', 'welcome');
+
 Route::post('statuses', [StatusesController::class, 'store'])->name('statuses.store')->middleware('auth');
 
 
---
+++ tests/Browser/UsersCanCreateStatusesTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class UsersCanCreateStatusesTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @test
+     * @throws \throwable
+     */
+    public function users_can_create_statuses()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->type('body', 'Mi primer status')
+                ->press('#create-status')
+                ->assertSee('Mi primer status');
+        });
+    }
+}
